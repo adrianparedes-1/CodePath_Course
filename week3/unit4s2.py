2550da4b215105ea1d6d77ea8c23e8fd50debab6
Adrian Paredes <adrianparedesroman99@gmail.com>
2024-10-16 11:58:13 -0400

    working on session 2 problem set 1

--- week3/unit4s2.py
+++ week3/unit4s2.py
@@ -1,28 +1,35 @@
 #1
-'''
-understand: write a function that takes two strings as parameters and checks if the the string has been long pressed
-Long pressed means that every character in the first string is in the second string in the same position
-
-plan: check if name is in typed -- this will immediately tell us if it is long pressed
-If it is not in typed then return false
-we can do this by making a dictionary but we will use the two pointer method
-have a pointer that starts at the beginning of name and another that starts at typed
-
-have a counter variable that keeps track of frequency in name and typed. make sure count1 <= count2
-a
-count1 = 1
-a
-count2 = 1
-
-l
-count1 = 1
-a
-count2 = 2
-
-e
-count1 = 1
-l
-count2 = 1
-'''
 def is_long_pressed(name, typed):
-    pass
+    ptr1 = 0
+    ptr2 = 0
+
+    while ptr2 < len(typed):
+        # If characters match, move both pointers
+        if ptr1 < len(name) and name[ptr1] == typed[ptr2]:
+            ptr1 += 1
+        # If typed character matches the previous typed character, just move ptr2
+        elif ptr2 > 0 and typed[ptr2] == typed[ptr2 - 1]:
+            pass  # Allow long press
+        else:
+            return False  # Mismatch
+        ptr2 += 1  # Always move ptr2 forward
+
+    # Check if all characters in name were matched
+    return ptr1 == len(name)
+
+# Test cases
+name1 = "alex"
+typed1 = "aaleex"
+print(is_long_pressed(name1, typed1))  # True
+
+name2 = "saeed"
+typed2 = "ssaaedd"
+print(is_long_pressed(name2, typed2))  # False
+
+name3 = "courtney"
+typed3 = "courtney"
+print(is_long_pressed(name3, typed3))  # True
+
+
+
+#2
